7d7dea17dafe84de6bf98e54cfeef254691cff1b
kqdn <kdn.k1m2006@gmail.com>
2026-03-11 15:20:46 -0700

    add displayprojects functoin

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,20 @@ let projects = [
 }
 
 ]
+
+function displayProjects() {
+    let projectsContainer = document.getElementById("projects-container");
+    console.log(projectsContainer);
+    projects.forEach(project => {
+        let projectElement = document.createElement("div");
+        projectElement.classList.add("project");
+        projectElement.innerHTML = `
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+            <a href="${project.link}" target="_blank">View Project</a>
+        `;
+        projectsContainer.appendChild(projectElement);
+    });
+}
+
+displayProjects();
