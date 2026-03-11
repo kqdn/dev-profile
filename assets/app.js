ce9b2da94b35c44ae28c61cfb61b699a96e1ca23
kqdn <kdn.k1m2006@gmail.com>
2026-03-11 15:03:31 -0700

    arr of projects

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+let projects = [
+
+{
+    title: "Project 1",
+    description: "Description of project 1",
+    link: ""
+},
+
+{
+    title: "Project 2",
+    description: "Description of project 2",
+    link: ""
+}
+
+]
